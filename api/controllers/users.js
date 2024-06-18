154505d7697e0be5ea9f109bb766a8b2f1ed31d2
Brunno Morokuma <BrunnoMorokuma1234@Gmail.com>
2024-06-18 14:08:16 -0300

    finished

---
+++ api/controllers/users.js
@@ -0,0 +1,31 @@
+import { db } from "../db.js";
+export const getUsers = (_, res) => {
+  const q = "SELECT * FROM agenda";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.status(200).json(data);
+  });
+};
+export const addUser = (req, res) => {
+  const q = "INSERT INTO agenda (`nome`, `telefone`) VALUES(?)";
+  const values = [req.body.nome, req.body.telefone];
+  db.query(q, [values], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Registro criado com sucesso");
+  });
+};
+export const updateUser = (req, res) => {
+  const q = "UPDATE agenda SET `nome` = ?, `telefone` = ? WHERE `id` = ?";
+  const values = [req.body.nome, req.body.telefone];
+  db.query(q, [...values, req.params.id], (err) => {
+    if (err) return res.jason(err);
+    return res.status(200).json("Registro atualizado com sucesso");
+  });
+};
+export const deleteUser = (req, res) => {
+  const q = "DELETE FROM agenda WHERE `id` = ?";
+  db.query(q, [req.params.id], (err) => {
+    if (err) return res.json(err);
+    return res.status(200).json("Registro deletado com sucesso");
+  });
+};
---
+++ api/db.js
@@ -0,0 +1,7 @@
+import mysql from "mysql";
+export const db = mysql.createConnection({
+host: "localhost",
+user: "root",
+password: "mysql4@11",
+database: "dbagenda",
+});
